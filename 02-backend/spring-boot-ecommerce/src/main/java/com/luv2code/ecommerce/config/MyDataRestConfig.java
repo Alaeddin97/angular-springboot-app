cbbb2ee02f7e7238316979f71e089b73c12ff9ea
GARRAOUI <alaeddine.garraoui@cgi.com>
2022-11-07 13:19:44 +0100

    add angular frontend

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -2,11 +2,12 @@ package com.luv2code.ecommerce.config;
 
 import com.luv2code.ecommerce.entity.Product;
 import com.luv2code.ecommerce.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
+@Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
@@ -15,7 +16,7 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(Product.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(notAllowedActions)))
                 .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(notAllowedActions)));
-       
+
         config.getExposureConfiguration()
                 .forDomainType(ProductCategory.class)
                 .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(notAllowedActions)))
--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/repository/ProductRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/repository/ProductRepository.java
@@ -3,7 +3,6 @@ package com.luv2code.ecommerce.repository;
 import com.luv2code.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 
 @RepositoryRestResource(collectionResourceRel = "Product")
 public interface ProductRepository extends JpaRepository<Product, Long> {
