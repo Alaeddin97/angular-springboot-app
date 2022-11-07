040908c1492bc7a7c739ae75a9fbf0a052ef5044
GARRAOUI <alaeddine.garraoui@cgi.com>
2022-11-07 11:14:29 +0100

    first commit

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,22 @@
+package com.luv2code.ecommerce.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Table(name = "category")
+@Entity
+@Getter
+@Setter
+public class ProductCategory {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "category_name")
+    private String categoryName;
+    @OneToMany(cascade = CascadeType.ALL,mappedBy = "category")
+    private Set<Product>products;
+}
